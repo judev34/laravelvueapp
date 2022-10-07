735ce9609c536fe2293fdbd3dd6cf626ad1b271a
Julien Coudert <julien.coudert34@gmail.com>
2022-10-08 00:19:03 +0200

    Merge branch 'breeze-test'

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import router from './router/index';
+import CustomerIndex from './components/CustomerIndex.vue';
+
+createApp({
+    components: {
+        CustomerIndex,
+    }
+}).use(router).mount('#app');
+
+
---
+++ resources/js/components/CustomerIndex.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divie-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    COL
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    COL
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    COL
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    COL
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    COL
+                                </th>
+                                <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">EDIT</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-10 w-10">
+                                            
+                                        </div>
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                MaryJane
+                                            </div>
+                                            <div class="text-sm text-gray-500">
+                                                mary.jane@example.com
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import useCustomer from '../services/CustomerService.js'
+
+export default {
+    setup() {
+
+        const { customers, getCustomers } = useCustomer();
+
+        onMounted(getCustomers());
+
+        return {
+            customers
+        }
+    }
+}
+
+</script>
