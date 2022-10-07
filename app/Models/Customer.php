4bbb5807f8ac38a91c796588c075e909dd2c4415
Julien Coudert <julien.coudert34@gmail.com>
2022-10-07 17:47:39 +0200

    feat(vue): import vue / clean alpine bug / storage

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    // preciser type de modèle à renvoyer
+    // on veut forcer le boolean
+    protected $casts = [
+        'is_favorite' => 'boolean',
+    ];
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'is_favorite',
+    ];
 }
